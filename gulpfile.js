2486f49b9a117bcd6e1c41351cc81d17934671f3
鼠大憨 <n_sdhan@3songshu.com>
2019-12-31 22:01:47 +0800

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var htmlclean = require('gulp-htmlclean');
+var babel = require('gulp-babel');
+var imagemin = require('gulp-imagemin');
+// 压缩 public 目录 css
+gulp.task('minify-css', function() {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss())
+        .pipe(gulp.dest('./public'));
+    done();
+});
+// 压缩 public 目录 html
+gulp.task('minify-html', function() {
+  return gulp.src('./public/**/*.html')
+    .pipe(htmlclean())
+    .pipe(htmlmin({
+         removeComments: true,
+         minifyJS: true,
+         minifyCSS: true,
+         minifyURLs: true,
+    }))
+    .pipe(gulp.dest('./public'));
+    done();
+});
+// 压缩 public/js 目录 js
+gulp.task('minify-js', function() {
+    return gulp.src('./public/**/*.js')
+        .pipe(babel({
+             presets: ['es2015']
+         }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./public'));
+    done();
+});
+gulp.task('minify-images', function () {
+    return gulp.src('./public/images/**/*.*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('./public/images'));
+    done();
+});
+// 执行 gulp 命令时执行的任务
+gulp.task('default', gulp.series('minify-html', 'minify-css', 'minify-js'));
